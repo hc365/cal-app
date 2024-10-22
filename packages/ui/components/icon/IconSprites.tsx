18316db2d2da8ace90de8dfeb2cdebaffb5458ff
Victor Mattos <victor@veploy.com>
2024-10-22 18:06:49 -0300

    fix: icons subdomains

--- packages/ui/components/icon/IconSprites.tsx
+++ packages/ui/components/icon/IconSprites.tsx
@@ -2,11 +2,7 @@ import * as React from "react";
 import SVG from "react-inlinesvg";
 
 export function IconSprites() {
-  return (
-    <SVG
-      src={`${process.env.NEXT_PUBLIC_WEBAPP_URL}/icons/sprite.svg?v=${process.env.NEXT_PUBLIC_CALCOM_VERSION}`}
-    />
-  );
+  return <SVG src={`/icons/sprite.svg?v=${process.env.NEXT_PUBLIC_CALCOM_VERSION}`} />;
 }
 
 export default IconSprites;
